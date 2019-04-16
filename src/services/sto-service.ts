f045a8ae2eff0560f13008b250864e79ebe9a5c1
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-16 21:14:04 +1000

    feat(services): kyc methods and upload token for endpoint

--- src/services/sto-service.ts
+++ src/services/sto-service.ts
@@ -16,12 +16,61 @@ export class StoService {
         });
     }
 
-    kycStatus(username: string) {
-        this.http.fetch('kyc/status', {
+    async uploadRequest(username: string) {
+        const res = await this.http.fetch(`uploadRequestToken/${username}`);
+
+        return await res.json();
+    }
+
+    /**
+     * Get the KYC status for a logged in user
+     * 
+     * @param username 
+     */
+    async kycStatus(username: string) {
+        const res = await this.http.fetch('kyc/status', {
             method: 'POST',
             body: json({
                 username
             })
-        })
+        });
+
+        return await res.json();
+    }
+
+    /**
+     * Upload KYC data to the server for a specific user
+     * 
+     * @param username 
+     * @param body 
+     */
+    async kycUpload(username: string, body: any) {
+        const res = await this.http.fetch('kyc/submit', {
+            method: 'POST',
+            body: json({
+                username,
+                ...body
+            })
+        });
+
+        return await res.json();
+    }
+
+    /**
+     * Update KYC data to the server for a specific user
+     * 
+     * @param username 
+     * @param body 
+     */
+    async kycUpdate(username: string, body: any) {
+        const res = await this.http.fetch('kyc/save', {
+            method: 'POST',
+            body: json({
+                username,
+                ...body
+            })
+        });
+
+        return await res.json();
     }
 }
