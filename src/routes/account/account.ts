d5e896a308456d0f8b46d139af2d81f3c721f9dd
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-16 21:13:38 +1000

    feat(account): if local username found

--- src/routes/account/account.ts
+++ src/routes/account/account.ts
@@ -1,3 +1,17 @@
+import { StoService } from './../../services/sto-service';
+import { autoinject } from 'aurelia-framework';
+
+@autoinject()
 export class Account {
-    
+    constructor(private stoService: StoService) {
+
+    }
+
+    activate() {
+        const username = localStorage.getItem('username');
+
+        if (username) {
+            
+        }
+    }
 }
