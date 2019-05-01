c52b4eaa23a12dd6eb7180dec23ff93c8ddbbea6
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-05-01 16:59:33 +1000

    fix(value-converters): auth filter return if no data supplied

--- src/resources/value-converters/auth-filter.ts
+++ src/resources/value-converters/auth-filter.ts
@@ -3,6 +3,10 @@ import { valueConverter } from 'aurelia-binding';
 @valueConverter('authFilter')
 export class AuthFilter {
   toView(routes, loggedIn) {
+    if (!routes) {
+        return routes;
+    }
+
     if (loggedIn) {
       return routes.filter(r => !r.config.publicOnly);
     }
