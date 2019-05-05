1c75d496b48bae44ecd9fc3463ea64a3c9481b1a
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-05-05 11:22:08 +1000

    feat(environment): add disabled tokens

--- aurelia_project/environments/base.ts
+++ aurelia_project/environments/base.ts
@@ -1,5 +1,6 @@
 export const baseEnvironmentConfiguration = {
     siteName: 'Steem Engine.',
     defaultLocale: 'en',
-    GRAPHQL_API: 'https://graphql.steem.services/'
+    GRAPHQL_API: 'https://graphql.steem.services/',
+    DISABLED_TOKENS: ['BTC', 'STEEM', 'SBD', 'BCC'],
 };
