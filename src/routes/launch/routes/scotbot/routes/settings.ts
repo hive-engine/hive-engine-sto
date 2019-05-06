fed22e3e059266083d6d7d4f276f1a5ff7bf6d39
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-05-06 16:51:55 +1000

    feat(scotbot): settings template

--- src/routes/launch/routes/scotbot/routes/settings.ts
+++ src/routes/launch/routes/scotbot/routes/settings.ts
@@ -1,3 +1,37 @@
+import { ValidationController, ValidationControllerFactory } from "aurelia-validation";
+import { BootstrapFormRenderer } from "resources/bootstrap-form-renderer";
+import { SteemEngine } from "services/steem-engine";
+import { autoinject } from "aurelia-framework";
+import environment from "environment";
+@autoinject()
 export class Settings {
-    
+    private controller: ValidationController;
+    private renderer: BootstrapFormRenderer;
+
+    private environment = environment;
+
+    private balance;
+    private tokens = [];
+
+    constructor(private controllerFactory: ValidationControllerFactory, private se: SteemEngine) {
+        this.controller = controllerFactory.createForCurrentScope();
+
+        this.renderer = new BootstrapFormRenderer();
+
+        this.controller.addRenderer(this.renderer);
+    }
+
+    async activate() {
+        const user = localStorage.getItem('username');
+
+        try {
+            const balance = await this.se.loadBalances(user, environment.NATIVE_TOKEN);
+
+            if (balance[0]) {
+                this.balance = parseFloat(balance[0].balance).toFixed(3);
+            }
+        } catch { /* none */ }
+        
+        this.tokens = await this.se.loadUserTokens(user);
+    }
 }
