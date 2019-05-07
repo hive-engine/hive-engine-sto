84d6679367a9e1498693f6f2f0365cd1c0479ce2
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-05-07 21:34:21 +1000

    chore(interfaces): scot info

--- custom_typings/scotbot.d.ts
+++ custom_typings/scotbot.d.ts
@@ -17,3 +17,18 @@ interface ScotConfig {
     vote_power_consumption?: number;
     vote_regeneration_seconds?: number;
 }
+
+interface ScotInfo {
+    claimed_token?: number;
+    enabled?: boolean;
+    last_reduction_block_num?: number;
+    last_reward_block_num?: number;
+    pending_rshares?: number;
+    pending_token?: number;
+    reward_pool?: number;
+    rewards_token?: number;
+    setup_complete?: number;
+    start_block_num?: number;
+    symbol?: string;
+    total_generated_token?: number;
+}
