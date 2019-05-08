9a42fa9b5273113526897dafa4e1b625f0d07aa2
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-05-08 15:28:18 +1000

    refactor(resources): object keys value converter rewritten

--- src/resources/value-converters/object-keys.ts
+++ src/resources/value-converters/object-keys.ts
@@ -1,6 +1,9 @@
-export class ObjectKeysValueConverter {
+export class KeysValueConverter {
     toView(obj) {
-        return Array.from(Object.keys(obj), k => obj[k]);
+        if (obj !== null && typeof obj === 'object') {
+            return Reflect.ownKeys(obj).filter(x => x !== '__observers__');
+        } else {
+            return null;
+        }
     }
-
 }
