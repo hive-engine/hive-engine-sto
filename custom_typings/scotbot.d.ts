1462d632f58cd9a4e8f2555e1ddf3d0d22b8d86c
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-05-08 15:27:56 +1000

    chore(typings): commenting interface for scot

--- custom_typings/scotbot.d.ts
+++ custom_typings/scotbot.d.ts
@@ -12,23 +12,37 @@ interface ScotConfig {
     reduction_percentage?: number;
     rewards_token?: number;
     rewards_token_every_n_block?: number;
+
+    /** The symbol of the token. eg. ENG */
     token?: string;
+
+    /** Who is the token owner? */
     token_account?: string;
+
     vote_power_consumption?: number;
     vote_regeneration_seconds?: number;
 }
 
 interface ScotInfo {
     claimed_token?: number;
+
+    /** A boolean that determines if the token is enabled or not */
     enabled?: boolean;
+
     last_reduction_block_num?: number;
     last_reward_block_num?: number;
     pending_rshares?: number;
     pending_token?: number;
     reward_pool?: number;
     rewards_token?: number;
+
+    /** Starts with -1, after first 500 changes to 0, after first 250 changes to 1 and second 250 transfer is changed to 2 */
     setup_complete?: number;
+
     start_block_num?: number;
+
+    /** The symbol of the token. eg. ENG */
     symbol?: string;
+
     total_generated_token?: number;
 }
