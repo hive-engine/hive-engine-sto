e72f302ed315b56c5009ab8bcf283170eb4c51bd
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-05-30 08:59:25 +1000

    chore(interfaces): type definitions for config

--- custom_typings/interfaces.d.ts
+++ custom_typings/interfaces.d.ts
@@ -15,11 +15,31 @@ interface EnvironmentInterface {
     ACCOUNTS_API_URL: string;
     CONVERTER_API: string;
     NODE_API_URL: string;
+    SCOT_API_URL: string;
     GRAPHQL_API: string;
+    PRICE_API: string;
     SCOT_API: string;
     STEEMP_ACCOUNT: string;
     NATIVE_TOKEN: string;
     DISABLED_TOKENS: string[];
     PEGGED_TOKEN: string;
     PEGGED_TOKENS: PeggedToken[];
+    SCOTBOT: {
+        FEE_ACCOUNT: string;
+        CHANGE_ACCOUNT: string;
+        FEES: {
+            INITIAL: string;
+            SETUP_1: string;
+            SETUP_2: string;
+            CHANGE: string;
+        }
+        PUBLIC_KEY: string;
+        FEE_ACCOUNT_1: string;
+        FEE_ACCOUNT_2: string;
+    };
+    NITROUS: {
+        FEE_ACCOUNT: string;
+        FEE: number;
+        FEE_SYMBOL: string;
+    }
 }
