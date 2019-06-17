cdbae3cc4443aaa7bb6b360dacc11b9458ebe6ce
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-06-18 09:03:51 +1000

    feat(environment): airdrop, typed native tokens

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,7 @@
 import { baseEnvironmentConfiguration } from 'base-environment';
+import { NativeTokens } from 'common/types';
 
-export default {
+export const environment: Partial<EnvironmentInterface> = {
     ...baseEnvironmentConfiguration,
     debug: false,
     testing: false,
@@ -12,31 +13,14 @@ export default {
     PRICE_API: 'https://postpromoter.net/api/prices',
 	ACCOUNTS_API_URL: 'https://api.steem-engine.com/accounts',
     CONVERTER_API: 'https://converter-api.steem-engine.com/api',
-	NATIVE_TOKEN: 'ENG',
-	STEEMP_ACCOUNT: 'steem-peg',
-	PEGGED_TOKEN: 'STEEMP',
-	PEGGED_TOKENS: [
-		{
-			name: 'Bitcoin',
-			symbol: 'BTC',
-			pegged_token_symbol: 'BTCP'
-		}, 
-		{
-			name: 'Litecoin',
-			symbol: 'LTC',
-			pegged_token_symbol: 'LTCP'
-		}, 
-		{
-			name: 'Bitcoin Cash',
-			symbol: 'BCH',
-			pegged_token_symbol: 'BCHP'
-		}, 
-		{
-			name: 'Dogecoin',
-			symbol: 'DOGE',
-			pegged_token_symbol: 'DOGEP'
-		}
-    ],
+	NATIVE_TOKEN: NativeTokens.Eng,
+    STEEMP_ACCOUNT: 'steem-peg',
+    AIRDROP: {
+        FEE_ACCOUNT: 'steem-eng',
+        FEE: '20.000',
+        MEMO: 'airdrop-fee',
+        TOKEN: NativeTokens.Eng
+    },
     SCOTBOT: {
         FEE_ACCOUNT: 'steemsc',
         CHANGE_ACCOUNT: 'null',
@@ -53,6 +37,6 @@ export default {
     NITROUS: {
         FEE_ACCOUNT: 'steem-eng',
         FEE: 200,
-        FEE_SYMBOL: 'ENG'
+        FEE_SYMBOL: NativeTokens.Eng
     }
-} as any;
+};
