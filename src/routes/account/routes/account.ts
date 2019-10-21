4d91f0179205bb01e8aae0ee45348a815ddd46d2
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-10-21 20:03:20 +1000

    feat(kyc): kyc route

--- src/routes/account/routes/account.ts
+++ src/routes/account/routes/account.ts
@@ -1,19 +1,27 @@
+import { log } from './../../../services/log-service';
 import { StoService } from '../../../services/sto-service';
 import { autoinject } from 'aurelia-framework';
 
 @autoinject()
 export class Account {
+    private hasCompletedKyc = false;
     private username;
     
     constructor(private stoService: StoService) {
 
     }
 
-    activate() {
+    async activate() {
         const username = localStorage.getItem('username');
 
         if (username) {
             this.username = username;
+
+            try {
+                const kyc = await this.stoService.kycStatus(username);
+            } catch (e) {
+                log.error(e);
+            }
         }
     }
 }
